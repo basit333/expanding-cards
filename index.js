468dfbc3eaa0c66b883633801c1d4b1a4c5b41a9
basit33 <shahbasit741@gmail.com>
2022-10-10 19:18:37 +0500

    made this project with html css javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+// Variables
+const panels = document.querySelectorAll(".panel");
+
+// Functions
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
